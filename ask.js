8e5faee8295014a4391f699c32da7d0eedb657af
Giorgi Ghughunishvili <g.ghughunishvili@gmail.com>
2023-04-30 19:06:33 -0400

    improved questions asking by using readline create interface;

--- ask.js
+++ ask.js
@@ -5,6 +5,29 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("What's your name? \n", name => {
-    console.log(`Hello ${name}!`);
+const questions = [
+    "What's your name?",
+    "Where do you live?",
+    "Why are you here?"
+];
+
+function collectAnswers(questions, done) {
+    const answers = [];
+
+    const questionAnswered = (answer) => {
+        answers.push(answer.trim());
+        if (answers.length < questions.length) {
+            rl.question(questions[answers.length] + "\t", questionAnswered);
+        } else {
+            return done(answers);
+        }
+    };
+
+    rl.question(questions[0] + "\t", questionAnswered);
+}
+
+collectAnswers(questions, (answers) => {
+    console.log("Thanks for using this app!");
+    console.log(answers);
+    process.exit();
 });
