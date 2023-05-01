a5865dc7f71282f6aa0ca6d1b5ca95cee94e5de6
Giorgi Ghughunishvili <g.ghughunishvili@gmail.com>
2023-04-30 21:14:03 -0400

    added event emitter example;

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const { inc, dec, getCount } = require("./myModule");
+// const { inc, dec, getCount } = require("./myModule");
 
-inc();
-inc();
-console.log(getCount());
-dec();
-console.log(getCount());
+// inc();
+// inc();
+// console.log(getCount());
+// dec();
+// console.log(getCount());
 
 // const myModule = require("./myModule");
 // console.log(myModule.name);
+const events = require("events");
+
+let emitter = new events.EventEmitter();
+
+const eventName = "customEvent";
+
+emitter.on(eventName, (message, user) => {
+    console.log(`${user}: ${message}`);
+});
+
+emitter.emit(eventName, "Wassup", "Gio");
+
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim();
+    if (input === "exit") {
+        emitter.emit(eventName, "GoodBye!", "process");
+        process.exit();
+    }
+    emitter.emit(eventName, data.toString().trim(), "terminal");
+});
