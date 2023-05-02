d8968816e91f6ccb2cf2414e9ed0d0746d832e14
Giorgi Ghughunishvili <g.ghughunishvili@gmail.com>
2023-05-01 21:24:35 -0400

    added modularization of collecAnswers;

--- ask.js
+++ ask.js
@@ -1,30 +1,10 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
     "What's your name?",
     "Where do you live?",
     "Why are you here?"
 ];
-
-function collectAnswers(questions, done) {
-    const answers = [];
-
-    const questionAnswered = (answer) => {
-        answers.push(answer.trim());
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length] + "\t", questionAnswered);
-        } else {
-            return done(answers);
-        }
-    };
-
-    rl.question(questions[0] + "\t", questionAnswered);
-}
 
 collectAnswers(questions, (answers) => {
     console.log("Thanks for using this app!");
